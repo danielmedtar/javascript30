65e184351259d4af5453d3fd0e6731d6a85eba9f
danielmedtar <dmedinat.ar@gmail.com>
2023-02-13 15:44:51 -0300

    ejercicio 2: Reloj con CSS y JS

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
-    const key = document.querySelector(`.key[data-key="${e.code}"]`);
-    
-    if(!audio) return;
-    
-    audio.currentTime = 0;
-    audio.play()
-    key.classList.add('playing')
+const segundero = document.querySelector('.second-hand')
+const minutero = document.querySelector('.min-hand')
+const hora = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date()
+    const horas = now.getHours()
+    const minutos = now.getMinutes()
+    const segundos = now.getSeconds()
+
+    const horaDegrees = ((horas / 12) * 360) + 90
+    const minutosDegrees = ((minutos / 60) * 360) + 90
+    const segundosDegrees = ((segundos / 60) * 360) + 90
+
+    hora.style.transform = `rotate(${horaDegrees}deg)`
+    minutero.style.transform = `rotate(${minutosDegrees}deg)`
+    segundero.style.transform = `rotate(${segundosDegrees}deg)`
 }
 
-function removeTransition(e) {
-    if(e.propertyName != 'transform') return;
-    this.classList.remove('playing')
-}
-
-const keys = document.querySelectorAll('.key')
-
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
-
-window.addEventListener('keydown', playSound)
+setInterval(setDate, 1000)
