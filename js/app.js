ee37065481a09bf2cd951691c2d7eb5a108533e3
danielmedtar <dmedinat.ar@gmail.com>
2023-03-27 15:11:45 -0300

    Dibujando con canvas

--- js/app.js
+++ js/app.js
@@ -1,70 +1,64 @@
- // ## Array Cardio Day 2
+const canvas = document.querySelector('#draw');
 
- const people = [
-    { name: 'Wes', year: 1988 },
-    { name: 'Kait', year: 1986 },
-    { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
-  ];
+const ctx = canvas.getContext('2d')
 
-  const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+//Seteamos el canvas del mismo ancho y alto de la pantalla del usuario
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-  //? Some and Every Checks
-  //ToDo Array.prototype.some() // is at least one person 19 or older?
-  //*Forma Larga
-//   const adulto = people.some(function(persona) {
-//     const currentYear = new Date().getFullYear()
-//     if(currentYear - persona.year >= 19) {
-//         return true
-//     }
-//   })
+//Seteamos estilos de la línea que se dibuja
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
 
-  //*Forma Corta
-//   const adulto = people.some(persona => ((new Date()).getFullYear()) - persona.year >= 19)
-//   console.log(adulto);
+// Variables iniciales
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
 
-  //ToDo Array.prototype.every() // is everyone 19 or older?
-//   const todosMayores = people.every(persona => ((new Date()).getFullYear()) - persona.year >= 19)
+// Funcion para detectar si se debe dibujar o no
 
-//   console.log({todosMayores});
+function draw(e) {
+  if(!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+
+  ctx.beginPath();
+  // Empieza desde aca
+  ctx.moveTo(lastX, lastY);
+  // Va hasta
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+  hue++
+
+  if(hue >= 360) {
+    hue = 0
+  }
+
+  if(ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+    direction = !direction
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+
+  console.log(direction);
+}
 
 
-  //? Array.prototype.find()
-  // Find is like filter, but instead returns just the one you are looking for
-  //ToDo Encuentra el comentario con el ID 823423
-    //*Forma larga
-//   const comment = comments.find(function(comment) {
-//     if(comment.id === 823423) {
-//         return true
-//     }
-//   })
+//Eventos eventos del mouse para dibujar
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+});
 
-    //*Forma corta
-//     const comment = comments.find(comment => comment.id === 823423)
-//   console.log(comment);
-
-  // Array.prototype.findIndex()
-  // Find the comment with this ID
-  // delete the comment with the ID of 823423
-
-  const index = comments.findIndex(comment => comment.id === 823423)
-
-// Sacando el comentario del array original:
-// comments.splice(index, 1)
-console.table(comments);
-
-// Creando un nuevo array con la info editada
-
-const newComments = [
-    ...comments.slice(0, index),
-    ...comments.slice(index + 1)
-]
-  console.table(newComments);
-
-  
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
